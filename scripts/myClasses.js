cf72d2f2a8cc95f8d123eb817cfef2cac62898d6
Shane Rhoads <shane@rhoads.com>
2018-04-13 16:15:27 -0400

    Fix button hooking

--- scripts/myClasses.js
+++ scripts/myClasses.js
@@ -24,7 +24,7 @@ function insertClass(className, classID, numStudents, numSessions){
     var goButton = document.createElement("button");
     goButton.type = "button";
     goButton.className = "btn-custom";
-    goButton.onclick = "gotoClass("+classID+")";
+    goButton.onclick = gotoClass(classID);
 
     // Class element
     var classElement = document.createElement("div");
