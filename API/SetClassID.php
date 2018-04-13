f57cd3f4d0032e127f75657e56b6b087c45a75d7
Thomas Anchor <30919196+TAnchor@users.noreply.github.com>
2018-04-13 16:09:13 -0400

    Update SetClassID.php

--- API/SetClassID.php
+++ API/SetClassID.php
@@ -1,7 +1,8 @@
 <?php
-	// Assumes the input is a JSON file in the format of {"session":"", "classID":""}
+	// Assumes the input is a JSON file in the format of {"session":"", "classID":"", "className":""}
 	// Sets the PHP session classID value to the value specified in the input JSON.
 	// This allows the classID value to be used for other API endpoints, like creating sessions
+	// className is optional, but allows the name to be returned in GetInfo.php
 	
 	$inData = getRequestInfo();
 	
@@ -11,12 +12,13 @@
 	if ($session != ""){
 		session_id($session);
 	}
-	if (!session_start()){
-		returnWithError("Unable to access session");
+	if(!session_start()){
+		returnWithError("Could not find session.");
 		exit();
 	}
 	
 	$_SESSION["classID"] = $classID;
+	$_SESSION["className"] = $className;
 	
 	// Removes whitespace at the front and back, and removes single quotes and semi-colons
 	function trimAndSanitize($str){
